0c82e9e9ad05338f723d5ff6f94c78a4f8b349b8
oliviaram <olivia.ramos@ontariotechu.net>
2022-02-02 19:09:50 -0500

    Added Apache Commons CSV as a dependency and a source file

--- lab2/src/main/java/csci2020u/lab02/SimpleTest.java
+++ lab2/src/main/java/csci2020u/lab02/SimpleTest.java
@@ -2,7 +2,7 @@ package csci2020u.lab02;
 
 public class SimpleTest {
     public String getGreeting() {
-        return "Hell World!";
+        return "Hello World!";
     }
 
     public static void main(String[] args) {
